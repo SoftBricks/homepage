51f5b659068bdc0fcdbb87c45bafd9d7ecf6e595
Lukas Sporrer <lukas.sporrer@softbricks.de>
2017-12-20 18:27:03 +0100

    change logo

--- src/components/softbricks-logo.js
+++ src/components/softbricks-logo.js
@@ -3,7 +3,7 @@ import softbricks from "../../public/static/softbricks.svg";
 
 const SoftBricksLogo = () =>
   <div className="logo">
-    <img height={50} src={softbricks} alt="Softbricks" />
+    <img height={22} src={softbricks} alt="Softbricks" />
   </div>;
 
 export default SoftBricksLogo;
