aa7363e1213bc4d1dc81f648473c1ee35dd83f2c
philippspo <philipp.sporrer@commercetools.de>
2017-12-22 00:51:25 +0100

    fix(footer): add contentful attribution

--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -6,6 +6,7 @@ import Nav from "../components/nav";
 import Text from "../components/text";
 import Center from "../components/center";
 import Inset from "../components/inset";
+import Stack from "../components/stack";
 import Toolbar from "../components/toolbar";
 import colors from "../constants/colors";
 
@@ -16,7 +17,7 @@ const FooterContainer = styled.footer`
 const Copyright = styled(Text.Detail)`
   padding: 32px 0 0;
   color: ${colors.white5};
-`
+`;
 
 export default class Footer extends Component {
   render() {
@@ -30,7 +31,20 @@ export default class Footer extends Component {
             </div>
           </Toolbar>
           <Center>
-            <Copyright>© 2017 SoftBricks. All Rights Reserved</Copyright>
+            <Stack alignItems="center" scale="xl">
+              <Copyright>© 2017 SoftBricks. All Rights Reserved</Copyright>
+              <a
+                href="https://www.contentful.com/"
+                rel="nofollow"
+                target="_blank"
+              >
+                <img
+                  src="https://images.contentful.com/fo9twyrwpveg/7Htleo27dKYua8gio8UEUy/0797152a2d2f8e41db49ecbf1ccffdaa/PoweredByContentful_DarkBackground_MonochromeLogo.svg"
+                  style={{ maxWidth: 100, width: '100%' }}
+                  alt="Powered by Contentful"
+                />
+              </a>
+              </Stack>
           </Center>
         </ResponsiveContainer>
       </FooterContainer>
