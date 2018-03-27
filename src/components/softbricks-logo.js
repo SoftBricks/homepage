5b9050d405bef21192003689ea6f6bdcc02e5a15
philippspo <philipp.sporrer@commercetools.de>
2018-03-27 11:00:26 +0200

    refactor(logo): wrap in link

--- src/components/softbricks-logo.js
+++ src/components/softbricks-logo.js
@@ -1,9 +1,18 @@
 import React from "react";
+import Link from "gatsby-link";
+import styled from "styled-components";
 import softbricks from "../../public/static/softbricks.svg";
 
+const LogoLink = styled(Link)`
+  color: white;
+  text-decoration: none;
+`;
+
 const SoftBricksLogo = () =>
-  <div className="logo">
-    <img height={22} src={softbricks} alt="Softbricks" />
-  </div>;
+  <LogoLink to="/">
+    <div className="logo">
+      <img height={22} src={softbricks} alt="Softbricks" />
+    </div>
+  </LogoLink>;
 
 export default SoftBricksLogo;
