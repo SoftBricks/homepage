f72ab7166b6af5e107e3287a2984e6be6e0fca9d
philippspo <philipp.sporrer@commercetools.de>
2018-03-27 11:00:41 +0200

    feat(datenschutz): add datenschutz text

--- src/constants/colors.js
+++ src/constants/colors.js
@@ -2,7 +2,8 @@ const colors = {
   orange: "#ef891c",
   black: "#151517",
   white: "hsl(20, 7%, 91%)",
-  white5: "hsl(20, 7%, 50%)"
+  white5: "hsl(20, 7%, 50%)",
+  white3: "hsl(20, 7%, 30%)"
 };
 
 export default colors;
--- src/layouts/footer.js
+++ src/layouts/footer.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import styled from "styled-components";
+import Link from "gatsby-link";
 import SoftBricksLogo from "../components/softbricks-logo";
 import ResponsiveContainer from "../components/responsive-container";
 import Nav from "../components/nav";
@@ -18,6 +19,12 @@ const Copyright = styled(Text.Detail)`
   padding: 32px 0 0;
   color: ${colors.white5};
 `;
+const DatenschutzLink = styled(Link)`
+  text-decoration: none;
+  :visited {
+    color: ${colors.white3};
+  }
+`
 
 export default class Footer extends Component {
   render() {
@@ -32,7 +39,7 @@ export default class Footer extends Component {
           </Toolbar>
           <Center>
             <Stack alignItems="center" scale="xl">
-              <Copyright>© 2017 SoftBricks. All Rights Reserved</Copyright>
+              <Copyright>© 2017 SoftBricks. All Rights Reserved | <DatenschutzLink to="/datenschutz">Datenschutzerklärung</DatenschutzLink></Copyright>
               <a
                 href="https://www.contentful.com/"
                 rel="nofollow"
