812d036be39b03f748ca85e20d128912079abe64
philippspo <phil.sporrer@gmail.com>
2018-10-09 11:58:32 +0200

    chore(graphql): import graphql

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import styled from "styled-components";
 import Img from "gatsby-image";
+import { graphql } from "gatsby"
 import Center from "../components/center";
 import ResponsiveContainer from "../components/responsive-container";
 import Inset from "../components/inset";
